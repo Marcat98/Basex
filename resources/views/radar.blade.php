7473515fa240fe5f01384b5e1b3c217610352f0a
mc4594s <marvin.catkhada@alumni.fh-aachen.de>
2022-01-18 16:24:44 +0100

    Ring view

--- resources/views/radar.blade.php
+++ resources/views/radar.blade.php
@@ -7,15 +7,19 @@
     <div id="container"></div>
 </figure>
 
-<button class="btn" onclick="exportAsPng()" style="width:10%;margin-top:0%;background-color: #8C8C8C;">
+<button class="btn" onclick="exportAsPng()" style="width:10%;margin-top:10%;background-color: #8C8C8C;">
   <span>Export</span></button>
 
 @if(App\Models\User::where('username',session()->get('user'))->first()->isModerator())
 <a href="{{ route('editRadar', ['radarId' => request()->radarId]) }}">
-  <button class="btn" style="width:10%;margin-top:27%;background-color: #8C8C8C;">
+  <button class="btn" style="width:10%;margin-top:2%;background-color: #8C8C8C;">
     <span>Edit Project</span></button>
 </a>
 @endif
+
+<fieldset id ="levels" style="margin-top:5%; margin-right:1%">
+<legend>Ring:</legend>
+</fieldset>
 
 <script>
   var data = {!! $data !!};
@@ -31,6 +35,18 @@
   chart.container("container");
   // initiate drawing the chart
   chart.draw();
+
+  //Ring View
+  var maxDepth = chart.getStat('treeMaxDepth');
+  for (var i = 0; i <= maxDepth; i++) {
+  	var enabled = chart.level(i).enabled();
+  	$('#levels').append('<input type="checkbox" checked="true"  name="levels" id="level ' + i + '" value="' + i + '" ' + (enabled ? 'checked' : '') + '>');
+  	$('#levels').append('<label for="' + i + '">Ring ' + (i+1) + '</label><br>');
+  }
+
+  $('input[name=levels]').on('change', function() {
+  	chart.level(+$(this).val()).enabled($(this).is(':checked'));
+  });
 
   function exportAsPng() {
       // Saves into PNG file.
